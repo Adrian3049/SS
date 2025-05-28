08e74aac43f364d970922b4e3230c2321953ea20
Adrian3049 <bmae14092007@gmail.com>
2025-05-28 10:58:26 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+Window.onload = function() {
+ver reply = prompt("Bienvenido ¿cual es tu nombre?", "")
+alert ( "Es un gusto tenerte aqui" + reply + "¡")
+}
+let miboton = document.querySelector('button');
+miboton.onclick = function() {
+   alert("Con el codigo: JTOS obtendras un 5% en tu proxima compra");
+}
